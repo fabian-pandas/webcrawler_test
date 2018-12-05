05aa8d4631c766ab46214e9b163f4232b5be3b6c
fabian-pandas <33372478+fabian-pandas@users.noreply.github.com>
2018-12-05 14:46:42 +0100

    Update execute.py

--- execute.py
+++ execute.py
@@ -1,9 +1,13 @@
 form fetcher import *
 
+
+# Klasse zuordnen
 crawledarticle = ArticleFetcher()
 crawledarticle.fetch()
 results = crawledarticle.fetch()
 
+
+# Reulstate ausgeben lassen
 for result in results:
     print(result.emoji + ": " + result.title)
 
