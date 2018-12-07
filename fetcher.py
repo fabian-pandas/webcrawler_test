ba92058cad11a2e80cbea03a5053a61f64b3bf91
fabian-pandas <33372478+fabian-pandas@users.noreply.github.com>
2018-12-07 16:18:45 +0100

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -10,28 +10,34 @@ class ArticleFetcher():
     
     def fetch(self):
         web_url = "http://python.beispiel.programmierenlernen.io/index.php"
-        time.sleep(1)
-        print(web_url)
-        r = requests.get(web_url)
-        doc = BeautifulSoup(r.text, "html.parser")
-        
         articles = []
-        for i in range(2):
-            if len(articles) == 0:
-                for card in doc.select(".card"):
-                    emoji = card.select_one(".emoji").text
-                    content = card.select_one(".card-text").text
-                    title = card.select(".card-title span")[1].text
-                    image = urljoin(web_url, card.select_one("img").attrs["src"])
+        while web_url !="":
+            time.sleep(1)
+            print(web_url)
+            r = requests.get(web_url)
 
-                    crawled = CrawledArticle(title, emoji, content, image)
-                    articles.append(crawled)
+             doc = BeautifulSoup(r.text, "html.parser")
+             
+             for card in doc.select(".card"):
+                emoji = card.select_one(".emoji").text
+                content = card.select_one(".card-text").text
+                title = card.select(".card-title span")[1].text
+                image = urljoin(web_url, card.select_one("img").attrs["src"])
 
+                crawled = CrawledArticle(title, emoji, content, image)
+                articles.append(crawled)
+            
+             next_button = doc.select_one(".navigation .btn") 
+             if next_button:
+                next_href = next_button.attrs["href"]
+                next_href = urljoin(web_url, next_href)
+                print(next_href)
+                web_url = next_href
             else:
-                # bessere Lösung möglich? ev. len()? range soll die Anzahl der Seiten angeben!
-                for a in range(6):
-                    for url in doc.find_all('a', href = True):
-                        new_url = urljoin(web_url,url.get('href'))
+                web_url = ""
+        
+        return articles
+    
 
                     r = requests.get(new_url)
                     doc = BeautifulSoup(r.text, "html.parser")
