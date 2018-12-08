6512e279f597720a6a1fce453251793cf34a533a
fabian-pandas <33372478+fabian-pandas@users.noreply.github.com>
2018-12-08 17:02:18 +0100

    Update fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -1,3 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import time
+import csv
+
+
 class CrawledArticle():
     def __init__(self, title, emoji, content, image):
         self.title = title
